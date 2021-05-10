dbbe066aa5e0b36a9adacec612995e1f447a53f8
marek-sz <marekszymkowiak1@gmail.com>
2021-05-10 13:16:20 +0200

    add DirReduction

--- src/test/java/main/codewars/directionsreductions/DirReductionTest.java
+++ src/test/java/main/codewars/directionsreductions/DirReductionTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertArrayEquals;
 public class DirReductionTest {
     @Test
     public void testSimpleDirReduc() {
+        assertArrayEquals("\"NORTH\",\"WEST\",\"SOUTH\",\"EAST\"", new String[]{"NORTH", "WEST", "SOUTH", "EAST"}, DirReduction.dirReduc(new String[]{"NORTH", "WEST", "SOUTH", "EAST"}));
         assertArrayEquals("\"NORTH\", \"SOUTH\", \"SOUTH\", \"EAST\", \"WEST\", \"NORTH\", \"WEST\"",
                 new String[]{"WEST"},
                 DirReduction.dirReduc(new String[]{"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"}));
