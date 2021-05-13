cd6619dff6ec8d5ba5d5cd56fc74808ae73ba8f0
marek-sz <marekszymkowiak1@gmail.com>
2021-05-13 11:58:01 +0200

    add The Office I - Outed

---
+++ src/main/java/main/codewars/theoffice1/Person.java
@@ -0,0 +1,11 @@
+package main.codewars.theoffice1;
+
+class Person {
+    final String name;
+    final int happiness;
+
+    public Person(String name, int happiness) {
+        this.name = name;
+        this.happiness = happiness;
+    }
+}
---
+++ src/main/java/main/codewars/theoffice1/TheOffice.java
@@ -0,0 +1,16 @@
+package main.codewars.theoffice1;
+
+import java.util.Arrays;
+
+public class TheOffice {
+    private static final String GOOD_JOB = "Nice Work Champ!";
+    private static final String BAD_JOB = "Get Out Now!";
+
+    public static String outed(Person[] meet, String boss) {
+        double averageHappiness = Arrays.stream(meet)
+                .mapToInt(p -> p.name.equals(boss) ? p.happiness * 2 : p.happiness)
+                .average()
+                .orElse(0.0);
+        return averageHappiness <= 5 ? BAD_JOB : GOOD_JOB;
+    }
+}
