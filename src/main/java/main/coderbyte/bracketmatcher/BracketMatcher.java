9d9ac94cb1700210e88b5041f1f1dcd8bda35e39
marek-sz <marekszymkowiak1@gmail.com>
2021-05-21 13:17:26 +0200

    codersbyte BracketMatcher add stackApproach method

--- src/main/java/main/coderbyte/bracketmatcher/BracketMatcher.java
+++ src/main/java/main/coderbyte/bracketmatcher/BracketMatcher.java
@@ -1,5 +1,6 @@
 package main.coderbyte.bracketmatcher;
 
+import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,5 +15,31 @@ public class BracketMatcher {
         }
 
         return formatted.isEmpty() ? 1 : 0;
+    }
+
+    public static int stackApproach(String string) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            if (stack.isEmpty()) {
+                switch (c) {
+                    case '(':
+                        stack.push(c);
+                        break;
+                    case ')':
+                        return 0;
+                }
+            } else {
+                switch (c) {
+                    case '(':
+                        stack.push(c);
+                        break;
+                    case ')':
+                        stack.pop();
+                        break;
+                }
+            }
+        }
+        return stack.isEmpty() ? 1 : 0;
     }
 }
--- src/test/java/main/coderbyte/bracketmatcher/BracketMatcherTest.java
+++ src/test/java/main/coderbyte/bracketmatcher/BracketMatcherTest.java
@@ -10,4 +10,10 @@ public class BracketMatcherTest {
         assertEquals(0, BracketMatcher.bracketMatcher("(coder)(byte))"));
         assertEquals(1, BracketMatcher.bracketMatcher("(c(oder)) b(yte)"));
     }
+
+    @Test
+    public void stackApproachTest() {
+        assertEquals(0, BracketMatcher.stackApproach("(coder)(byte))"));
+        assertEquals(1, BracketMatcher.stackApproach("(c(oder)) b(yte)"));
+    }
 }
